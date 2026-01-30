74a6b3ab71ffab00c289ae5c90279b854f8aeb4d
Aditya Cahyadi Sujanadi <aditya.cahyadi.29@gmail.com>
2026-01-30 11:24:20 +0700

    add: getAdmin

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,3 +1,20 @@
-const getAdmin = async () => {};
+const service = require("../services/adminService");
+
+const getAdmin = async (req, res) => {
+  try {
+    const data = await service.getAdmin(req.query);
+
+    return res.status(200).json({
+      status: 200,
+      message: "Successfully retrieve admins.",
+      data: data,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      message: "Internal server error.",
+    });
+  }
+};
 
 module.exports = { getAdmin };
--- services/adminService.js
+++ services/adminService.js
@@ -1,5 +1,28 @@
 const db = require("../database");
 
-const getAdmin = async () => {};
+const getAdmin = async (httpQuery) => {
+  let query = "SELECT * FROM admin";
+  let params = [];
+  const httpQueryLength = Object.keys(httpQuery).length;
+
+  if (httpQueryLength > 0) {
+    query += " WHERE ";
+    Object.entries(httpQuery).forEach(([key, value], idx) => {
+      params.push(value);
+      query += `${key} = ?`;
+      if (idx === httpQueryLength - 1) {
+        query += "";
+      } else {
+        query += ", ";
+      }
+    });
+  }
+
+  //   console.log(query);
+  //   console.log(params);
+
+  const [rows] = await db.query(query, params);
+  return rows;
+};
 
 module.exports = { getAdmin };
