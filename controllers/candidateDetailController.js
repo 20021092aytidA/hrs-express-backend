f387638c043986f9264b1842f19db86dc8f717bb
Aditya Cahyadi Sujanadi <aditya.cahyadi.29@gmail.com>
2026-02-04 11:30:23 +0700

    add: POST candidate_detail

--- controllers/candidateDetailController.js
+++ controllers/candidateDetailController.js
@@ -17,7 +17,68 @@ const getCandidateDetail = async (req, res) => {
   }
 };
 
-const postCandidateDetail = async (req, res) => {};
+const postCandidateDetail = async (req, res) => {
+  const {
+    position_name,
+    current_company,
+    current_company_location,
+    expected_salary,
+    current_salary,
+    resume,
+    added_by,
+  } = req.body;
+
+  try {
+    if (
+      !position_name ||
+      !current_company ||
+      !current_company_location ||
+      !expected_salary ||
+      !current_salary ||
+      !resume ||
+      !added_by
+    ) {
+      return res
+        .status(400)
+        .json({ status: 400, message: "Bad request, missing body value(s)." });
+    }
+
+    const postRes = await service.postCandidateDetail(
+      position_name,
+      current_company,
+      current_company_location,
+      expected_salary,
+      current_salary,
+      resume,
+      added_by,
+    );
+
+    if (!postRes) {
+      return res.status(500).json({
+        status: 500,
+        message: "Failed to create new candidate detail.",
+      });
+    }
+
+    return res.status(201).json({
+      status: 201,
+      message: "Succesfully created new candidate detail.",
+      data: {
+        position_name: position_name,
+        current_company: current_company,
+        current_company_location: current_company_location,
+        expected_salary: expected_salary,
+        current_salary: current_salary,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      message: "Internal server error.",
+      error: error,
+    });
+  }
+};
 
 const patchCandidateDetail = async (req, res) => {};
 
--- services/candidateDetailService.js
+++ services/candidateDetailService.js
@@ -1,4 +1,5 @@
 const db = require("../database");
+const dateHelper = require("../helper/date");
 
 const getCandidateDetail = async (httpQuery) => {
   let query = "SELECT * FROM candidate_detail";
@@ -21,7 +22,31 @@ const getCandidateDetail = async (httpQuery) => {
   return candidateDetails;
 };
 
-const postCandidateDetail = async () => {};
+const postCandidateDetail = async (
+  positionName,
+  currentCompany,
+  currentCompanyLocation,
+  expectedSalary,
+  currentSalary,
+  resume,
+  addedBy,
+) => {
+  const [post] = await db.query(
+    `INSERT INTO candidate_detail (position_name, current_company, current_company_location, expected_salary, current_salary, resume, created_by, created_at) VALUES (?,?,?,?,?,?,?,?)`,
+    [
+      positionName,
+      currentCompany,
+      currentCompanyLocation,
+      expectedSalary,
+      currentSalary,
+      resume,
+      addedBy,
+      dateHelper.getDateTime(),
+    ],
+  );
+
+  return post.affectedRows > 0 ? true : false;
+};
 
 const patchCandidateDetail = async () => {};
 
